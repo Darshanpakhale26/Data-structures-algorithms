b3fdadc47df199b64c4db93bf341cc870b9371f3
Darshan Pakhale <darshanvpakhale26@gmail.com>
2024-02-08 13:29:25 +0530

    Initial Commit

---
+++ Strings/reverse_string.cpp
@@ -0,0 +1,20 @@
+// Program to reverse a string
+
+#include <iostream>
+#include <bits/stdc++.h> // include this header file to use reverse function
+using namespace std;
+
+void reverseString(string str) // function to reverse the string
+{
+    reverse(str.begin(), str.end()); // using inbuilt function to reverse the string
+    cout << str;                     // printing the reversed string
+}
+int main()
+{
+    string str = "mystring"; // initializing the string
+    reverseString(str);           // calling the function to reverse the string
+
+    return 0;
+}
+
+// Output: gnirtsym
