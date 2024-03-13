65a423fe77cd71547e6ea944d2b5da2aba185bdb
Darshan Pakhale <darshanvpakhale26@gmail.com>
2024-03-13 15:34:30 +0530

    Initial Commit

---
+++ Arrays/remove_duplicates.cpp
@@ -0,0 +1,57 @@
+// Program to remove duplicates from an array
+
+// #include <iostream>
+// #include <unordered_map>
+// #include <vector>
+
+// using namespace std;
+
+// vector<int> remove_duplicates(vector<int> arr) {
+//     unordered_map<int, int> map;                // creating a map
+//     for (int i = 0; i < arr.size(); i++) {
+//         map[arr[i]]++;                          // inserting elements in map
+//     }
+//     vector<int> res;
+//     for (auto i : map) {                        // iterating through the map
+//         res.push_back(i.first);                 // inserting the elements in the result vector
+//     }
+//     return res;
+// }
+
+// int main() {
+//     vector<int> arr = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6};
+//     vector<int> res = remove_duplicates(arr);
+//     for (auto i : res) {
+//         cout << i << " ";
+//     }
+//     return 0;
+// }
+
+// 
+
+// Using set
+
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+vector<int> remove_duplicates(vector<int> arr) {
+    set<int> s;                                 // creating a set
+    for (int i = 0; i < arr.size(); i++) {
+        s.insert(arr[i]);                       // inserting elements in set
+    }
+    vector<int> res(s.begin(), s.end());        // converting set to vector
+    return res;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6};
+    vector<int> res = remove_duplicates(arr);
+    for (auto i : res) {
+        cout << i << " ";
+    }
+    return 0;
+}
+
