53b85c4476bb820c1675d0292ca21204934b3bf7
Darshan Pakhale <darshanvpakhale26@gmail.com>
2024-04-06 12:15:31 +0530

    Initial Commit

--- Arrays/non_repeating.cpp
+++ Arrays/non_repeating.cpp
@@ -1,4 +1,32 @@
-// Program to find noo repeating element in an array
+// Program to find non repeating element in an array
+
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+
+// using namespace std;
+
+// int non_repeating(vector<int> arr) {
+//     unordered_map<int, int> mp;             // Create a map to store the frequency of each elemen
+//     for (int i = 0; i < arr.size(); i++) {
+//         mp[arr[i]]++;                       // Count the frequency of each element
+//     }
+//     for (int i = 0; i < arr.size(); i++) {
+//         if (mp[arr[i]] == 1) {              // If frequency is 1, return the element
+//             return arr[i];
+//         }
+//     }
+//     return -1;
+// }
+
+// int main() {
+//     vector<int> arr = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5};
+//     cout << non_repeating(arr) << endl;
+//     return 0;
+// }
+
+
+// Program to find first non repeating element in an array {-1, 2, -1, 3, 0}
 
 #include <iostream>
 #include <vector>
@@ -6,8 +34,8 @@
 
 using namespace std;
 
-int non_repeating(vector<int> arr) {
-    unordered_map<int, int> mp;             // Create a map to store the frequency of each elemen
+int first_non_repeating(vector<int> arr) {
+    unordered_map<int, int> mp;             // Create a map to store the frequency of each element
     for (int i = 0; i < arr.size(); i++) {
         mp[arr[i]]++;                       // Count the frequency of each element
     }
@@ -20,7 +48,7 @@ int non_repeating(vector<int> arr) {
 }
 
 int main() {
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5};
-    cout << non_repeating(arr) << endl;
+    vector<int> arr = {-1, 2, -1, 3, 0};
+    cout << first_non_repeating(arr) << endl;
     return 0;
 }
