1db3da23ae8b5eeb5d5a0830d53e8c2787dee447
Darshan Pakhale <darshanvpakhale26@gmail.com>
2024-04-07 00:24:14 +0530

    Initial Commit

---
+++ Arrays/upper_diagonal_element.cpp
@@ -0,0 +1,31 @@
+// Program to make the upper diagonal elements of a matrix zero
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void makeUpperDiagonalZero(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            matrix[i][j] = 0;                    // making the upper diagonal elements zero
+        }
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    makeUpperDiagonalZero(matrix);
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
