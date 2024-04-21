e4aaf53b6e8c1183f1ab0fc856e8640d4ce81034
Darshan Pakhale <darshanvpakhale26@gmail.com>
2024-04-21 16:48:48 +0530

    Initial Commit

---
+++ Strings/camelCase.cpp
@@ -0,0 +1,28 @@
+// Given a sentence cstr, written in camel case, 
+//The task is to print the words of the sentence separately. in lower case.
+// Example:
+// Input: camelCase
+// Output: camel case
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void camelCase(string str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (isupper(str[i])) {
+            cout << " ";
+        }
+        cout << (char)tolower(str[i]);
+    }
+}
+
+int main() {
+    string str;
+    cout << "Enter a string in camel case: ";
+    getline(cin, str);
+    cout << "The sentence in lower case: ";
+    camelCase(str);
+    cout << endl;
+    return 0;
+}
